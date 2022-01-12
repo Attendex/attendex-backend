34a2c26fa60b4179dd2212d90d90c50398793f1a
Ryan Walter Lee <88182652+ryanwalterlee@users.noreply.github.com>
2022-01-12 21:15:31 +0800

    change update member attendance request body

--- routes/memberattendance/updatememberattendance.js
+++ routes/memberattendance/updatememberattendance.js
@@ -4,21 +4,12 @@ var verifyJWT = require("./../../src/verifyJWT");
 
 var db = require('../../src/connection');
 
-// requires a body with attended, memberid, bookid and date properties
+// requires a body with attended, memberid, sheetid properties
 
 router.put('/', verifyJWT, async function(req, res, next) {
 
-  let sheetID;
-
-  // get new sheetid
-  let sql = `SELECT sheetID FROM attendancesheet
-    WHERE bookID = ${req.body.bookid} AND ${req.body.date}`;
-  await db.promise().query(sql)
-    .then((result => {sheetID = result[0][0].sheetID}))
-    .catch(err => {throw err})
-
   sql = `UPDATE memberattendance SET attended = ${req.body.attended} 
-    WHERE memberID = ${req.body.memberid} AND fk_sheetID = ${sheetID}`;
+    WHERE memberID = ${req.body.memberid} AND fk_sheetID = ${req.body.sheetid}`;
   db.query(sql, (err, result) => {
     if (err) throw err;
   })
