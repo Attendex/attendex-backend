79d2d6744ad6150e152e7217831eecb1173d161e
Ryan Walter Lee <88182652+ryanwalterlee@users.noreply.github.com>
2022-01-14 19:54:12 +0800

    added js extension to update book name

--- routes/book/updatebookname.js
+++ routes/book/updatebookname.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var verifyJWT = require("./../../src/verifyJWT");
+var verifyJWT = require("../../src/verifyJWT");
 
 var db = require('../../src/connection');
 
